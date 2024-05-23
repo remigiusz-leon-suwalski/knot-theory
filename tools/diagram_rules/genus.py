cb42aeaeff0f10f3df46b7408f48df07386b1cfa
Remigiusz L. Suwalski <remigiusz.suwalski@gmail.com>
2024-05-23 15:27:51 +0200

    Rewrite genus

--- tools/diagram_rules/genus.py
+++ tools/diagram_rules/genus.py
@@ -19,8 +19,8 @@ diagram_commands["GenusProofC"] = {
     "lines": [
         "\\draw[semithick,fill=diagramfiller] (-7, -5) to (-5, -5) [in=right, out=right] to (-5, 5) to (-7, 5);",
         "\\draw[semithick,fill=diagramfiller] ( 7, -5) to ( 5, -5) [in=left, out=left] to ( 5, 5) to (7, 5);",
-        "\\node at (-4.75, 0) {$M_1$};",
-        "\\node at (4.75, 0) {$M_2$};",
+        "\\node at (-4.75, 0) {$S_1$};",
+        "\\node at (4.75, 0) {$S_2$};",
     ],
     "sizes": ["Large"],
 }
@@ -29,7 +29,7 @@ diagram_commands["GenusProofD"] = {
         "\\fill[diagramfiller] (-5, -5) rectangle(5, 5);",
         "\\draw[semithick,fill=white] (-5, -5) to [out=right, in=left] (-2, -2) -- (2, -2) to [out=right, in=left] (5, -5);",
         "\\draw[semithick,fill=white] (-5,  5) to [out=right, in=left] (-2,  2) -- (2,  2) to [out=right, in=left] (5,  5);",
-        "\\node at (0, 0) {$M_{\\#}$};",
+        "\\node at (0, 0) {$S_{\\#}$};",
     ],
     "sizes": ["Large"],
 }
